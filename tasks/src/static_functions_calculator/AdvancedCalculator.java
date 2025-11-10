f86c98c6d2607b7b623ac184e671fb3bed2c1f66
Jonathan Bußhoff <jonathan@busshoff.net>
2025-11-10 15:46:55 +0100

    Refactor AdvancedCalculator for improved functionality

--- tasks/src/static_functions_calculator/AdvancedCalculator.java
+++ tasks/src/static_functions_calculator/AdvancedCalculator.java
@@ -1,93 +1,121 @@
-String[] getInput() {
-    String[] inputs = new String[3];
-    Scanner scanner = new Scanner(System.in);
-
-    System.out.println("Zahl 1:");
-    inputs[0] = scanner.nextLine();
-
-    System.out.println("Operator:");
-    inputs[1] = scanner.nextLine();
-
-    if (inputs[1].equals("!")) {
-        // Hier wird ein Platzhalterwert gesetzt, welcher aber nicht weiter verwendet wird.
-        inputs[2] = "-1";
-        return inputs;
-    }
-
-    System.out.println("Zahl 2:");
-    inputs[2] = scanner.nextLine();
-
-    return inputs;
-}
-
-double add(double summand1, double summand2) {
-    return summand1 + summand2;
-}
-
-double subtract(double minuend, double subtrahend) {
-    return minuend - subtrahend;
-}
-
-double multiply(double faktor1, double faktor2) {
-    return faktor1 * faktor2;
-}
-
-double divide(double dividend, double divisor) {
-    return dividend / divisor;
-}
-
-double mod(double dividend, double divisor) {
-    return dividend % divisor;
-}
-
-int faculty(int zahl) {
-    for (int i = zahl - 1; i > 0; i--) {
-        zahl *= i;
-    }
-    return zahl;
-}
-
-void output(double result) {
-    System.out.println("Ergebnis:");
-    System.out.println(result);
-}
-
 void main() {
-    String[] inputs = getInput();
-    double result;
+    double result = 0;
+    boolean useLastResult = false;
 
-    double a = Double.parseDouble(inputs[0]);
-    double b = Double.parseDouble(inputs[2]);
-    switch (inputs[1]) {
-        case "+":
-            result = add(a, b);
+    while (true) {
+        char operator = getOperatorInput();
+        if (operator == 'q') {
+            System.out.println("Der Taschenrechner wird beendet");
+            return;
+        }
+
+        if (operator == 'c') {
+            useLastResult = false;
+            result = 0;
+            System.out.println("Der Taschenrechner wurde zurückgesetzt");
+            continue;
+        }
+
+        if (operator != '+' && operator != '-' && operator != '/' && operator != '*' && operator != '%' && operator != '!') {
+            System.out.println("Dieser Operator ist nicht erlaubt. Versuchen Sie es erneut!");
+            continue;
+        }
+
+        double[] numbers = getNumberInput(operator, useLastResult, result);
+
+        result = calculate(operator, numbers);
+
+        printCalculation(numbers[0], numbers[1], operator, result);
+        useLastResult = true;
+    }
+}
+
+double calculate(char operator, double[] numbers) {
+    double result = 0;
+    switch (operator) {
+        case '+':
+            result = add(numbers[0], numbers[1]);
             break;
-        case "-":
-            result = subtract(a, b);
+        case '-':
+            result = subtract(numbers[0], numbers[1]);
             break;
-        case "*":
-            result = multiply(a, b);
+        case '*':
+            result = multiply(numbers[0], numbers[1]);
             break;
-        case "/":
-            if (b == 0) {
-                System.out.println("Eine Division durch Null ist nicht möglich");
-                return;
-            }
-            result = divide(a, b);
+        case '/':
+            result = divide(numbers[0], numbers[1]);
             break;
-        case "%":
-            result = mod(a, b);
+        case '%':
+            result = mod(numbers[0], numbers[1]);
             break;
-        case "!":
-            if (a < 0) {
-                System.out.println("Fakultät kann nur von positiven Zahlen bestimmt werden.");
-                return;
-            }
-            result = faculty((int) a);
+        case '!':
+            result = faculty(numbers[0]);
             break;
         default:
-            System.out.println("Dieser Operator ist nicht bekannt");
-            return;
+            System.out.println("Dieser operator ist nicht erlaubt");
+
     }
-    output(result);
+    return result;
+}
+
+double add(double zahl1, double zahl2) {
+    return zahl1 + zahl2;
+}
+
+double subtract(double zahl1, double zahl2) {
+    return zahl1 - zahl2;
+}
+
+double multiply(double zahl1, double zahl2) {
+    return zahl1 * zahl2;
+}
+
+double divide(double zahl1, double zahl2) {
+    if (zahl2 == 0) {
+        System.out.println("Durch 0 darf nicht geteilt werden.");
+        return 0;
+    }
+    return zahl1 / zahl2;
+}
+
+double mod(double zahl1, double zahl2) {
+    return zahl1 % zahl2;
+}
+
+double faculty(double zahl) {
+    int fact = 1;
+    for (int i = 1; i <= zahl; i++) {
+        fact = fact * i;
+    }
+    return fact;
+}
+
+
+double[] getNumberInput(char operator, boolean useResult, double result) {
+    Scanner inputScanner = new Scanner(System.in);
+    double[] numbers = new double[2];
+
+    if (useResult) {
+        numbers[0] = result;
+    } else {
+        System.out.println("Gebe Zahl 1 ein:");
+        numbers[0] = inputScanner.nextDouble();
+    }
+
+    if (operator != '!') {
+        System.out.println("Gebe Zahl 2 ein:");
+        numbers[1] = inputScanner.nextDouble();
+    }
+
+    return numbers;
+}
+
+char getOperatorInput() {
+    Scanner inputScanner = new Scanner(System.in);
+    System.out.println("Gebe den Operator ein (c um das vorherige Ergebnis zu löschen, q um das Programm zu beenden):");
+    return inputScanner.nextLine().charAt(0);
+}
+
+void printCalculation(double number1, double number2, char operator, double result) {
+    System.out.println(number1 + " " + operator + " " + number2 + " = " + result);
 }
